b41e93a0bbd6c52c34dce9bb7250d56b4718ad39
James Borrell <james.borrell92@gmail.com>
2016-02-04 09:16:45 +0000

    Gathering Data. Background.js, content.js & manifest.json

---
+++ background.js
@@ -0,0 +1,24 @@
+chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+  if (response === 'Twitter') {
+    saveTW();
+  };
+
+  if (response === 'Facebook') {
+    saveFB();
+  };
+
+});
+
+function saveFB(){
+  var facebook = new Date()/1000;
+  localStorage.setItem('FBtime', facebook);
+};
+
+function saveTW(){
+  var twitter = new Date()/1000;
+  localStorage.setItem('TWtime', twitter);
+};
+
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.create({"url": "time.html"})
+});
