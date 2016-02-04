feff4c011c4c43ee522ea4d080a847f07083a5c6
Adam Lancaster <lancaster28@hotmail.com>
2016-02-04 09:30:04 +0000

    integrating index.html, styles.css and animations.js

---
+++ animations.js
@@ -0,0 +1,37 @@
+var tenMins = document.getElementById("tenMins"),
+    thirtyMins = document.getElementById("thirtyMins"),
+    oneHour = document.getElementById("oneHour"),
+    originEndTime = new Date().getTime(),
+    originStartTime = new Date().getTime() - 1000,
+    thisPageStartTime = originEndTime - originStartTime,
+    mins = Math.floor(thisPageStartTime/60)-1,
+    thisSiteMins = -1;
+
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+};
+
+
+
+function addTime() {
+  thisSiteMins++;
+  thisSiteMins = checkTime(thisSiteMins);
+  document.getElementById("mins").innerHTML = thisSiteMins
+  setTimeout(addTime, 60000)
+};
+
+
+
+
+
+function hide(element) {
+  element.className += "hidden"
+}
+
+
+function show(element) {
+  element.classList.remove("hidden");
+
+}
